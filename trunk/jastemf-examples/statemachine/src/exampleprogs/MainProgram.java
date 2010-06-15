7bcb4790ef0d5b41d5a19cefafe2c5937fdf8c64
cwende <cwende@42a00c9d-b472-88f3-3bc5-ebb563497ae5>
2010-06-15 08:48:53 +0000

    prepared ast and graphial syntax to support actions and guards, adapted diagram code generation, added icons

--- trunk/jastemf-examples/statemachine/src/exampleprogs/MainProgram.java
+++ trunk/jastemf-examples/statemachine/src/exampleprogs/MainProgram.java
@@ -1,20 +1,27 @@
 package exampleprogs;
-import statemachine.impl.*;
+
+import statemachine.ast.Opt;
+import statemachine.impl.ActionExpressionImpl;
+import statemachine.impl.ActionImpl;
+import statemachine.impl.GuardExpressionImpl;
+import statemachine.impl.StateImpl;
+import statemachine.impl.StateMachineImpl;
+import statemachine.impl.TransitionImpl;
 
 public class MainProgram {
 
 	public static void main(String[] args) {
-	    // Construct the AST
-		StateMachineImpl m = new StateMachineImpl(); 
-		m.adddeclarations(new StateImpl("S1")); 
-		m.adddeclarations(new StateImpl("S2")); 
-		m.adddeclarations(new StateImpl("S3")); 
-		m.adddeclarations(new TransitionImpl("a", "S1", "S2")); 
-		m.adddeclarations(new TransitionImpl("b", "S2", "S1")); 
-		m.adddeclarations(new TransitionImpl("a", "S2", "S3")); 
+		// Construct the AST
+		StateMachineImpl m = new StateMachineImpl();
+		m.adddeclarations(new StateImpl("S1", new Opt<ActionImpl>(null)));
+		m.adddeclarations(new StateImpl("S2", new Opt<ActionImpl>(null)));
+		m.adddeclarations(new StateImpl("S3", new Opt<ActionImpl>(null)));
+		m.adddeclarations(new TransitionImpl("a", "S1", "S2", "", ""));
+		m.adddeclarations(new TransitionImpl("b", "S2", "S1", "", ""));
+		m.adddeclarations(new TransitionImpl("a", "S2", "S3", "", ""));
 
-	    // Print reachable information for all states
-	    m.printReachable();
+		// Print reachable information for all states
+		m.printReachable();
 
 	}
 
