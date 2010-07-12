5c8be7e7045311f37b0cc5f3e79822624c8ee121
christoff.Buerger <Christoff.Buerger@gmx.net>
2010-07-12 11:46:09 +0000

    Example repository cleanup.

--- jastemf-examples/statemachine/src/tests/StateMachineTests.java
+++ jastemf-examples/statemachine/src/tests/StateMachineTests.java
@@ -1,3 +1,11 @@
+/**
+ * <copyright>
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the BSD 3-clause license which accompanies this distribution.
+ *
+ * </copyright>
+ */
 package tests;
 
 import junit.framework.TestCase;
