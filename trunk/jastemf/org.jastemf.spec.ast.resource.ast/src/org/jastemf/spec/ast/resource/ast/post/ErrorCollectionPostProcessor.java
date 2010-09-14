068e5bcec9efb5856a48c06ab057f82000c50349
skarol@gmx.de <skarol@gmx.de@42a00c9d-b472-88f3-3bc5-ebb563497ae5>
2010-09-14 09:16:50 +0000

    added basic EMFText resource plugin including post processor

---
+++ trunk/jastemf/org.jastemf.spec.ast.resource.ast/src/org/jastemf/spec/ast/resource/ast/post/ErrorCollectionPostProcessor.java
@@ -0,0 +1,37 @@
+package org.jastemf.spec.ast.resource.ast.post;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.eclipse.emf.ecore.EObject;
+import org.jastemf.spec.ast.Grammar;
+import org.jastemf.spec.ast.impl.GrammarImpl;
+import org.jastemf.spec.ast.resource.ast.IAstOptionProvider;
+import org.jastemf.spec.ast.resource.ast.IAstOptions;
+import org.jastemf.spec.ast.resource.ast.IAstResourcePostProcessor;
+import org.jastemf.spec.ast.resource.ast.IAstResourcePostProcessorProvider;
+import org.jastemf.spec.ast.resource.ast.mopp.AstResource;
+
+public class ErrorCollectionPostProcessor implements 
+IAstResourcePostProcessor, IAstResourcePostProcessorProvider, IAstOptionProvider {
+
+	@Override
+	public void process(AstResource resource) {
+		EObject eo = resource.getContents().get(0);
+		if(eo!=null&&eo instanceof Grammar){
+			GrammarImpl grammar = (GrammarImpl)eo;
+			System.out.println(grammar.astErrors());
+		}
+	}
+
+	@Override
+	public IAstResourcePostProcessor getResourcePostProcessor() {
+		return this;
+	}
+
+	@Override
+	public Map<?, ?> getOptions() {
+		return Collections.singletonMap(IAstOptions.RESOURCE_POSTPROCESSOR_PROVIDER, this);
+	}
+
+}
