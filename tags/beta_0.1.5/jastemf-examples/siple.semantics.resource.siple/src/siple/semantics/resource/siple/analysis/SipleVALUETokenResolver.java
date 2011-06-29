ff68a5a186557262bb6bc1b58897a89262ce703d
skarol@gmx.de <skarol@gmx.de@42a00c9d-b472-88f3-3bc5-ebb563497ae5>
2011-06-29 08:45:07 +0000

    incrementing version 0.1.5

---
+++ tags/beta_0.1.5/jastemf-examples/siple.semantics.resource.siple/src/siple/semantics/resource/siple/analysis/SipleVALUETokenResolver.java
@@ -0,0 +1,28 @@
+/**
+ * <copyright>
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the BSD 3-clause license which accompanies this distribution.
+ *
+ * </copyright>
+ */
+package siple.semantics.resource.siple.analysis;
+
+public class SipleVALUETokenResolver implements siple.semantics.resource.siple.ISipleTokenResolver {
+	
+	private siple.semantics.resource.siple.analysis.SipleDefaultTokenResolver defaultTokenResolver = new siple.semantics.resource.siple.analysis.SipleDefaultTokenResolver();
+	
+	public java.lang.String deResolve(java.lang.Object value, org.eclipse.emf.ecore.EStructuralFeature feature, org.eclipse.emf.ecore.EObject container) {
+		java.lang.String result = defaultTokenResolver.deResolve(value, feature, container);
+		return result;
+	}
+	
+	public void resolve(java.lang.String lexem, org.eclipse.emf.ecore.EStructuralFeature feature, siple.semantics.resource.siple.ISipleTokenResolveResult result) {
+		defaultTokenResolver.resolve(lexem, feature, result);
+	}
+	
+	public void setOptions(java.util.Map<?,?> options) {
+		defaultTokenResolver.setOptions(options);
+	}
+	
+}
