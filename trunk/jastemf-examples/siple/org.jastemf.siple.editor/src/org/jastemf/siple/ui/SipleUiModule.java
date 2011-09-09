db11c22637f907879aa8b5fcd0e85fb8fbd56d64
sven.karol@googlemail.com <sven.karol@googlemail.com@42a00c9d-b472-88f3-3bc5-ebb563497ae5>
2011-09-09 14:43:05 +0000

    subclassed editor to enable updates of the properties view

--- trunk/jastemf-examples/siple/org.jastemf.siple.editor/src/org/jastemf/siple/ui/SipleUiModule.java
+++ trunk/jastemf-examples/siple/org.jastemf.siple.editor/src/org/jastemf/siple/ui/SipleUiModule.java
@@ -4,6 +4,7 @@
 package org.jastemf.siple.ui;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.eclipse.xtext.ui.editor.XtextEditor;
 
 /**
  * Use this class to register components to be used within the IDE.
@@ -12,4 +13,9 @@ public class SipleUiModule extends org.jastemf.siple.ui.AbstractSipleUiModule {
 	public SipleUiModule(AbstractUIPlugin plugin) {
 		super(plugin);
 	}
+	
+	 public Class<? extends XtextEditor> bindEditor() {
+		    return PropertiesViewAwareEditor.class;
+		  }
+	
 }
