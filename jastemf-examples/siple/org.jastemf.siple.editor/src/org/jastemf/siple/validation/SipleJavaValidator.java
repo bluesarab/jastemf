58a2823540e229d54d9acfd743b66420e77d543d
sven.karol <sven.karol@googlemail.com>
2011-10-05 08:14:49 +0000

    added error reporting for type analysis and name resolution

--- jastemf-examples/siple/org.jastemf.siple.editor/src/org/jastemf/siple/validation/SipleJavaValidator.java
+++ jastemf-examples/siple/org.jastemf.siple.editor/src/org/jastemf/siple/validation/SipleJavaValidator.java
@@ -1,13 +1,36 @@
 package org.jastemf.siple.validation;
+
+import org.eclipse.xtext.validation.Check;
+import org.jastemf.siple.impl.ExpressionImpl;
+import org.jastemf.siple.interfaces.Declaration;
+import org.jastemf.siple.interfaces.Expression;
+import org.jastemf.siple.interfaces.Reference;
+import org.jastemf.siple.meta.SiplePackage;
+import org.jastemf.siple.semantics.Type;
  
 
 public class SipleJavaValidator extends AbstractSipleJavaValidator {
 
-//	@Check
-//	public void checkGreetingStartsWithCapital(Greeting greeting) {
-//		if (!Character.isUpperCase(greeting.getName().charAt(0))) {
-//			warning("Name should start with a capital", MyDslPackage.Literals.GREETING__NAME);
-//		}
-//	}
+	@Check
+	public void checkType(Expression expression){
+		ExpressionImpl impl = (ExpressionImpl)(expression);
+		if(impl.getType()==Type.ERROR_TYPE){
+			error("Unable to determine the expression's type.",SiplePackage.Literals.EXPRESSION__TYPE);
+		}
+	}
+	
+	@Check
+	public void checkType(Declaration declaration){
+		if(declaration.getType()==Type.ERROR_TYPE){
+			error("Unable to determine the declarations's type.",SiplePackage.Literals.EXPRESSION__TYPE);			
+		}
+	}
+	
+	@Check
+	public void checkReference(Reference reference){
+		if(reference.getDeclaration()==null){
+			error("Unable to find declaration with name '" + reference.getName() + "'.",SiplePackage.Literals.REFERENCE__DECLARATION);
+		}
+	}
 
 }
